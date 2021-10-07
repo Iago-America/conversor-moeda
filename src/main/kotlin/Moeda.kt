6d38931145dc2fb035376e013b2e8c9b49b0d9f4
Iago América <iago6489@gmail.com>
2021-10-07 20:17:02 -0300

    Quinto Commit
    Modificação em algumas linhas de código e remoção da estruturação de decisão., a fim garantir uma boa visibilidade ao código.

--- src/main/kotlin/Moeda.kt
+++ src/main/kotlin/Moeda.kt
@@ -6,137 +6,78 @@ fun main() {
     print("""
         
         Bem vindo ao conversor de moedas criado por Iago.
-        Antes de começarmos você poderia digitar seu nome?
-        Sim ou Não?
+        Antes de começarmos fale seu nome.
         Digite: """.trimIndent())
 
-    val pergunta = readLine()!!
+        val name = readLine()
 
-    if (pergunta == Sim) {
-        print("\n"+"Digite seu nome: ")
-            val name = readLine()
+            print("""
+                |---------------------------------------
+                |Bem vindo $name! Espero que você esteja bem.
+                |Qual moeda você quer comprar?
+                |
+                |1 -> Dólar
+                |2 -> Euro
+                |3 -> Libra Esterlina
+                |4 -> Peso Argentino
+                |Digite: """.trimMargin())
 
-        print("""
-            |Bem vindo $name! Espero que você esteja bem.
-            |Qual moeda você quer comprar?
-            |
-            |1 -> Dólar
-            |2 -> Euro
-            |3 -> Libra Esterlina
-            |4 -> Peso Argentino
-            |Digite:""".trimMargin())
+            var x = Integer.valueOf(readLine())
 
-        var x = Integer.valueOf(readLine())
+            if (x == 1) {
+                print("""
+                    |---------------------------------------
+                    |Quantos doláres você pretende comprar?
+                    |Digite o valor: U$ """.trimMargin())
 
-        if (x == 1) {
-            print("\n" + "Quantos dolares você vai querer comprar?\n" +
-                    "Digite o valor: U$")
+                var a = readLine()!!.toFloat()
+                var b = 5.52
 
-            var a = readLine()!!.toFloat()
-            var b = 5.29
+                var resultado = a * b
 
-            var resultado = a * b
 
-            print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
+                print ("\n$name, você terá que pagar R$$resultado reais \n")
 
-        }else if (x == 2) {
-            print("\n" + "Quantos euros você vai querer comprar?\n" +
-                    "Digite o valor: €")
+            }else if (x == 2) {
+                print("""
+                    |---------------------------------------
+                    |Quantos euros você pretende comprar?
+                    |Digite o valor: €""".trimMargin())
 
-            var a = readLine()!!.toFloat()
-            var b = 6.18
+                var a = readLine()!!.toFloat()
+                var b = 6.38
 
-            var resultado = a * b
+                var resultado = a * b
 
-            print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
+                print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
 
-        }else if (x == 3) {
-            print("\n" + "Quantas libras esterlinas você vai querer comprar?\n" +
-                    "Digite o valor: ")
+            }else if (x == 3) {
+                print("""
+                    |---------------------------------------
+                    |Quantas libras esterlinas você pretende comprar?
+                    |Digite o valor: £""".trimMargin())
 
-            var a = readLine()!!.toFloat()
-            var b = 7.30
+                var a = readLine()!!.toFloat()
+                var b = 7.51
 
-            var resultado = a * b
+                var resultado = a * b
 
-            print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
+                print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
 
-        }else if (x == 4) {
-            print("\n" + "Quantos pesos argentinos você vai querer comprar?\n" +
-                    "Digite o valor: ")
+            }else if (x == 4) {
+                print("""
+                    |---------------------------------------
+                    |Quantas pesos argentinos você pretende comprar?
+                    |Digite o valor: """.trimMargin())
 
-            var a = readLine()!!.toFloat()
-            var b = 0.054
+                var a = readLine()!!.toFloat()
+                var b = 0.056
 
-            var resultado = a * b
+                var resultado = a * b
 
-            print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
+                print ("\n" + "$name, você terá que pagar R$$resultado reais\n")
 
-        }else {
-            print("Essa operação não existe! Escolha entre 1, 2, 3 e 4")
+            }else {
+                print("Essa operação não existe! Recomece e escolha entre 1, 2, 3 e 4")
+            }
         }
-    }
-
-    else if (pergunta == Não) {
-        print("\n"+"Tudo bem, respeito seu anonimato")
-
-        print("\n"+"Bem vindo! Espero que você esteja bem.\n" +
-                "Qual moeda você quer comprar?\n\n" +
-                "1 -> Dólar\n" +
-                "2 -> Euro\n" +
-                "3 -> Libra Esterlina\n" +
-                "4 -> Peso Argentino\n\n" +
-                "Digite: ")
-
-        var x = Integer.valueOf(readLine())
-
-        if (x == 1) {
-            print("\n" + "Quantos dolares você vai querer comprar?\n" +
-                    "Digite o valor: U$")
-
-            var a = readLine()!!
-            var b = 5.29
-
-            var resultado = a.toFloat() * b
-
-            print ("\n" + "Você vai precisar pagar R$$resultado reais\n")
-
-        }else if (x == 2) {
-            print("\n" + "Quantos euros você vai querer comprar?\n" +
-                    "Digite o valor: €")
-
-            var a = readLine()!!
-            var b = 6.18
-
-            var resultado = a.toFloat() * b.toFloat()
-
-            print ("\n" + "Você vai precisar pagar R$$resultado reais\n")
-
-        }else if (x == 3) {
-            print("\n" + "Quantas libras esterlinas você vai querer comprar?\n" +
-                    "Digite o valor: ")
-
-            var a = readLine()!!
-            var b = 7.30
-
-            var resultado = a.toFloat() * b.toFloat()
-
-            print ("\n" + "Você vai precisar pagar R$$resultado reais\n")
-
-        }else if (x == 4) {
-            print("\n" + "Quantos pesos argentinos você vai querer comprar?\n" +
-                    "Digite o valor: ")
-
-            var a = readLine()!!
-            var b = 0.054
-
-            var resultado = a.toFloat() * b.toFloat()
-
-            print ("\n" + "Você vai precisar pagar R$$resultado reais\n")
-
-        }else {
-            print("Essa operação não existe! Escolha entre 1, 2, 3 e 4")
-        }
-    }
-
-}
